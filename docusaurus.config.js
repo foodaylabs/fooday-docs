29a3c2a4fd6628423f48d6163f802abc341fe4e7
0xthk <kf013099@gmail.com>
2023-02-17 18:07:19 +0800

    fix: title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'FooDay',
-  tagline: 'Dinosaurs are cool',
+  title: 'Fooday',
+  tagline: 'Foodies United: Leave Reviews, Earn Rewards',
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
